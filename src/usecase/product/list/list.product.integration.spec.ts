42310b4a94f2e82e6ec5bb6f5a0ff273bd2141a5
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-03-23 21:53:50 -0300

    feat: add list products usecase

---
+++ src/usecase/product/list/list.product.integration.spec.ts
@@ -0,0 +1,49 @@
+import { Sequelize } from "sequelize-typescript";
+import Product from "../../../domain/product/entity/product";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import ListProductUseCase from "./list.product.usecase";
+
+describe("Test list products use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should list products", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new ListProductUseCase(productRepository);
+
+    const product = new Product("1", "Product A", 10);
+
+    await productRepository.create(product);
+
+
+    const output = {
+      products: [
+        {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+        }
+      ]
+    };
+
+    const result = await usecase.execute();
+
+    expect(result).toEqual(output);
+  });
+});
