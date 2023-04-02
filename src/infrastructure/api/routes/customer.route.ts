73ce75838e277881fc1475cabef08d9ad44204b5
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-04-01 22:31:10 -0300

    feat: add presenter on customer route

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import CustomerPresenter from "../presenters/customer.presenter";
 
 export const customerRoute = express.Router();
 
@@ -28,7 +29,11 @@ customerRoute.get("/", async (_: Request, res: Response) => {
   const usecase = new ListCustomerUseCase(new CustomerRepository());
   try {
     const output = await usecase.execute();
-    res.send(output);
+
+    res.format({
+      json: async () => res.send(output),
+      xml: async () => res.send(CustomerPresenter.listXML(output)),
+    });
   } catch (err) {
     res.status(500).send(err);
   }
